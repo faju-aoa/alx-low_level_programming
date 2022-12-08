809a1104acd46349b4f55f07d9dad88b8163646a
faju-aoa <unugorfelix@gmail.com>
2022-12-08 05:23:53 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/**
- *main - Entry point
- *
- *Return: Always 0 (Success)
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char string1[]= "\"Programming is like building a multilingual puzzle";
-  puts(string1);
-  return (0);
+    char string1="\"Programming is like building a multilingual puzzle";
+    puts(string1)
+    return (0);
 }

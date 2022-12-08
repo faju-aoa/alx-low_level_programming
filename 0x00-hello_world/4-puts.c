2fb2d663a67dc36f850dc462602c900ec87b9e9b
faju-aoa <unugorfelix@gmail.com>
2022-12-08 05:01:24 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,9 @@
-gcc alx_c.c -o alx_c
+#include <stdio.h>
+
+int main(void)
+{
+    char string1[]= "\"Programming is like building a multilingual puzzle \n";
+    puts(string1);
+    return (0);
+}
+

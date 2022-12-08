5d4be8d95bed3fd8183fbc3559a0859b93be43be
faju-aoa <unugorfelix@gmail.com>
2022-12-08 13:26:08 -0800

    Intel

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/**
- *main - Entry point
- *
- *Return: Always 0 (sucess)
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("I will work in Nasa");
-return (0);
+	printf("Faju-aoa School");
+	return (0);
 }
--- 0x00-hello_world/0-main.s
+++ 0x00-hello_world/0-main.s
@@ -2,7 +2,7 @@
 	.text
 	.section	.rodata
 .LC0:
-	.string	"I will work in Nasa"
+	.string	"Faju-aoa School"
 	.text
 	.globl	main
 	.type	main, @function

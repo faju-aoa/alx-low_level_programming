e6ef74051142c0d72c127971a70e775636c78591
faju-aoa <unugorfelix@gmail.com>
2022-12-08 06:58:32 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,10 @@
  *
  *Return: Always 0 (sucess)
  */
-
 int main(void)
 {
 
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art,");
 
 return (0);
 

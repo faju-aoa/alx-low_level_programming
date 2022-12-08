2636c5e44043fdd0918b2996444a41544ece9440
faju-aoa <unugorfelix@gmail.com>
2022-12-08 05:13:01 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    char string1[]= "\"Programming is like building a multilingual puzzle \n";
+    char string1[]= "\"Programming is like building a multilingual puzzle" ;
     puts(string1);
     return (0);
 }

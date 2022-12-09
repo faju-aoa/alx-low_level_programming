e6452984c22b62e82cc1827fd5f53c4f078ecdbb
faju-aoa <unugorfelix@gmail.com>
2022-12-09 15:10:28 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int i = 0;
+int j = 0;
+char string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char string1[] = "abcdefghijkmnopqrstuvwxyz\n";
+char letters1;
+char letters2;
+int lower;
+int upper;
+while (string[i])
+{
+while (string1[j])
+{
+letters1 = string[j];
+lower = tolower(letters1);
+putchar(lower);  
+j++;
+}
+letters2 = string[i];
+upper = toupper(letters2);
+putchar(upper);
+i++;
+}
+return (0);
+}

63e97f7a9bfb58d1a20d47dbc2cd6736bcd22339
faju-aoa <unugorfelix@gmail.com>
2022-12-13 12:52:52 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_alphabet_x1o - Prints "letter"
+ */
+void print_alphabet_x10(void)
+{
+int alph;
+int i;
+int letter;
+for (i = 0; i <= 9; i++)
+{
+for (alph = 'A'; alph <= 'Z'; alph++)
+{
+letter = alph;
+putchar(tolower(letter));
+}
+putchar('\n');
+} 
+}

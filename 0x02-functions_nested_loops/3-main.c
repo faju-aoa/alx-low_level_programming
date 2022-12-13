1d6c4b0a1fede87c909bfe2913b7c281db92ab96
faju-aoa <unugorfelix@gmail.com>
2022-12-13 15:13:39 -0800

    islower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "main.h"
-#include <unistd.h>
 /**
  * main - check the code.
  *
@@ -10,11 +10,11 @@ int main(void)
 int r;
 
 r = _islower('H');
-_putchar(r + '0');
+putchar(r + '0');
 r = _islower('o');
-_putchar(r + '0');
+putchar(r + '0');
 r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
+putchar(r + '0');
+putchar('\n');
 return (0);
 }

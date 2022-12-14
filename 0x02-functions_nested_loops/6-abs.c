9c36b902a1248895271c4bad672405c390947337
faju-aoa <unugorfelix@gmail.com>
2022-12-14 01:23:43 -0800

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *print_sign - check for sign of number
+ *@n:parameter for the function
+ *Return: Return 1 and 0 if n is +, 0 or -
+ */ 
+int _abs(int n)
+{
+int num;
+num = abs(n);
+_putchar('\n');
+return (num);
+}

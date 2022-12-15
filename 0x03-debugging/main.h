0ae219def2702ec37d975ce4d4ffb60b927c9e22
faju-aoa <unugorfelix@gmail.com>
2022-12-15 12:19:31 -0800

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-void positive_or_negative(int i);
+int positive_or_negative(int i);
 int largest_number(int a, int , int c);
 void print_remaining_days(int month, int day, int year);
 int conver_day(int month, int day);

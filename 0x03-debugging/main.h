63419440f4b2f9212268a219c433bbff61440d59
faju-aoa <unugorfelix@gmail.com>
2022-12-14 22:49:13 -0800

    Like, comment, subscribe

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,4 +3,8 @@
 
 #include <stdio.h>
 void positive_or_negative(int i);
+int largest_number(int a, int , int c);
+void print_remaining_days(int month, int day, int year);
+int conver_day(int month, int day);
+void print_times_table(int n);
 #endif

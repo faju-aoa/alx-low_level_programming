87d09aa310c906cae9efdc1fe1169776b826f627
faju-aoa <unugorfelix@gmail.com>
2022-12-20 03:27:35 -0800

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,12 @@ for (i = 0; i < n; i++)
 {
 if (i == 0)
 {
-printf ("%d", a[i]);
+printf("%d", a[i]);
 }
 else
 {
-printf (", %d", a[i]);
+printf(", %d", a[i]);
 }
 }
-printf ("\n");
+printf("\n");
 }

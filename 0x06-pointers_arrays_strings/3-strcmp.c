d2bcd28eb79c1d9d436bdfb8bd4f286e619bc6af
faju-aoa <unugorfelix@gmail.com>
2022-12-22 16:25:37 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strcmp - function that compares s1 and s2
+ *@s1:s1 pointer parameter;
+ *@s2:s2 pointer parameter
+ *Return: return void
+ */
+int _strcmp(char *s1, char *s2)
+{
+int p;
+p = strcmp(s1, s2);
+return (p);
+}

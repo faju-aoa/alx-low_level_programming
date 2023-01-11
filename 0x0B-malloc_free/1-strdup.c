a3248d348ca78a8e1b88970586c83400d48d742d
faju-aoa <unugorfelix@gmail.com>
2023-01-10 17:05:48 -0800

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,17 +15,15 @@ if (str == NULL)
 {
 return (NULL);
 }
-for (i = 0; str[i] != '\0'; i++)
-{
-}
-ptr = (char *)malloc(sizeof(char) * i);
+for (i = 0; str[i] != '\0'; i++);
+ptr = (char *)malloc(sizeof(char) * i + 1);
 if (ptr == NULL)
 {
 return (NULL);
 }
 for (j = 0; j < i; j++)
 {
-ptr[j] = *(str + j);
+ptr[j] = str[j];
 }
 return (ptr);
 }

003b1e29964a7ef5e90ba37325b1ec183ac8294b
faju-aoa <unugorfelix@gmail.com>
2023-01-12 12:13:00 -0800

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 void *ptr;
 char *f;
 unsigned int i;
-ptr = (char *)calloc(nmemb, size);
+ptr = (char *)malloc(nmemb * size);
 if (nmemb == 0 || size == 0)
 {
 return (NULL);

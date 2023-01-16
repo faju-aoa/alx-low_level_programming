908fa80aea22050e5845286c30b0323ee056044a
faju-aoa <unugorfelix@gmail.com>
2023-01-16 07:47:21 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ *struct dog - structure of the programe
+ *@name: store name of the dog
+ *@age: store age of the dog
+ *@owner: store owner of the dog
+ *
+ *Description: attribute of the dog
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog;
+#endif

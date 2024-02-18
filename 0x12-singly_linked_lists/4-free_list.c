3d442c79ab96184eef507f2b72df9f399f22926e
faju-aoa <unugorfelix@gmail.com>
2024-02-18 10:38:23 -0800

    4-commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include list.h;
+/**
+ * free_list - delete singly linked list node
+ * @head: head - head of the node
+ *
+ *Return void
+ */
+
+void free_list(list_t *head)
+{
+if (head == NULL)
+{
+printf("link is empty")
+}
+list_t *temp = head;
+head = head->next;
+free(head);
+temp = NULL;
+}

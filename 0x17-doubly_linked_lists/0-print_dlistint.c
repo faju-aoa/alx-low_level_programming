a3d76bb715d73a92218cd1c2e812a004226c1309
faju-aoa <unugorfelix@gmail.com>
2024-02-20 03:38:59 -0800

    0-commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,11 +6,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+size_t count = 0;
 dlistint_t *ptr = NULL;
 ptr = h;
 while (ptr != NULL)
 {
 printf("%d\n", ptr->n);
+count++;
 ptr = ptr->next;
 }
 return (ptr);

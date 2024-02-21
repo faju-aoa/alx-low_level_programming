3f2d5aa55fe86680af9e3091fd0b6db12ee5a330
faju-aoa <unugorfelix@gmail.com>
2024-02-21 06:16:15 -0800

    3-commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,14 +13,18 @@ dlistint_t *new_node;
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
+{
 new_node->n = n;
 new_node->next = NULL;
+}
 ptr = *head;
 if (ptr != NULL)
 {
 while (ptr->next != NULL)
+{
 ptr = ptr->next;
 ptr->next = new_node;
+}
 }
 else
 {

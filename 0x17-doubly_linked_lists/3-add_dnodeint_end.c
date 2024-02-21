96eeeb7f9de608eac31e3dae5b130114257acb34
faju-aoa <unugorfelix@gmail.com>
2024-02-21 13:31:58 -0800

    3-commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,33 +8,28 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-if (head == NULL)
-{
-return (NULL);
-}
-dlistint_t *temp = malloc(sizeof(dlistint_t));
+dlistint_t *ptr;
+dlistint_t *temp;
+temp = malloc(sizeof(dlistint_t));
 if (temp == NULL)
 {
 return (NULL);
 }
-if (*head == NULL)
-{
-temp->prev = NULL;
 temp->n = n;
 temp->next = NULL;
-(*head) = temp;
-return (*head);
-}
-temp->prev = NULL;
-temp->n = n;
-temp->next = NULL;
-dlistint_t *ptr = NULL;
 ptr = (*head);
+if (ptr != NULL)
+{
 while (ptr->next != NULL)
 {
 ptr = ptr->next;
 }
 ptr->next = temp;
+}
+else
+{
+(*head) = temp;
+}
 temp->prev = ptr;
-return (*head);
+return (ptr);
 }

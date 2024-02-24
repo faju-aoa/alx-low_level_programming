5c906efe41ef44a1d6ecbe58e8b107008371c72c
faju-aoa <unugorfelix@gmail.com>
2024-02-24 14:31:21 -0800

    5-commit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+  * get_dnodeint_at_index - get the index of the node
+  * @head: head of the node
+  * @index: count of the node
+  * Return: return ptr
+  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+int count = 0;
+dlistint_t *ptr = head;
+if (head == NULL)
+{
+return (NULL);
+}
+if (ptr == NULL)
+{
+return (NULL);
+}
+while (count < index)
+{
+if (ptr->next == NULL)
+{
+return (count);
+}
+count++;
+ptr = ptr->next;
+}
+return (ptr);
+}

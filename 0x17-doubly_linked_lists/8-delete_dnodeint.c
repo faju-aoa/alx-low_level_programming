fabd5e861b9ddae535dd93c739a5f5b7f0f79297
faju-aoa <unugorfelix@gmail.com>
2024-03-02 13:21:48 -0800

    8-commit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 dlistint_t *curr;
 unsigned int count;
 if (*head == NULL)
+{
 return (-1);
+}
 curr = *head;
 if (index == 0)
 {

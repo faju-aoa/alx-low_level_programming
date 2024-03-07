7869cb610b3bd6744be1f1fb6bc5dbbecafcec1f
faju-aoa <unugorfelix@gmail.com>
2024-03-07 12:14:00 -0800

    0-commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *ht;
+hash_table_t *hashTable;
 unsigned long int i;
-ht = malloc(sizeof(hash_table_t));
-if (ht == NULL)
+hashTable = malloc(sizeof(hash_table_t));
+if (hashTable == NULL)
 {
 return (NULL);
 }
-ht->size = size;
-ht->array = malloc(sizeof(hash_node_t *) * size);
-if (ht->array == NULL)
+hashTable->size = size;
+hashTable->array = malloc(sizeof(hash_node_t *) * size);
+if (hashTable->array == NULL)
 {
 return (NULL);
 }
 for (i = 0; i < size; i++)
 {
-ht->array[i] = NULL;
+hashTable->array[i] = NULL;
 }
-return (ht);
+return (hashTable);
 }
